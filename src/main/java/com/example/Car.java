45028a362e4c1cec4b3ac2918a4d8face3d837ee
waqar khan <waqarkhan9@yahoo.com>
2025-02-24 18:35:45 -0800

    car class added

--- src/main/java/com/example/Car.java
+++ src/main/java/com/example/Car.java
@@ -1,19 +1,33 @@
 package com.example;
 
+/*car class constructor 
+ * all the values are initialized here
+ */
 public class Car {
-    private String make;
-    private String model;
-    private int year;
-    private String color;
+    final private String make;
+    final private String model;
+    final private int year;
+    final private String color;
+
+    private int mileage;
+    private int engineSize;
+    private String gearType;
+
     
    public Car(String make, String model, int year, String color){
-
     this.make = make;
     this.model = model;
     this.year = year;
     this.color = color;
    }
 
-   
+   /*gives out car information */
+   public void getCarInfo(){
+    System.out.println("The information of this car is as follow" + "\n" +
+    make +   "\n" + model  + "\n" + + year + "\n" + color
+    );
+   }
+/*this method sets a new car values */
+
     
 }
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,6 +2,7 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car myCar = new Car("Toyota", "Corolla", 2022, "Blue");
+        myCar.getCarInfo();
     }
 }
