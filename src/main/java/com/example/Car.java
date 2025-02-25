32cda1980ed89375183ea0c861057f9a58e20b01
waqar khan <waqarkhan9@yahoo.com>
2025-02-24 19:05:56 -0800

    new code added

--- src/main/java/com/example/Car.java
+++ src/main/java/com/example/Car.java
@@ -3,28 +3,19 @@ package com.example;
 /*car class constructor 
  * all the values are initialized here
  */
-public class Car {
-    final private String make;
-    final private String model;
-    final private int year;
-    final private String color;
+public class Car extends Vehicle{
+    public Car(){
+        super(null, null, 0);    
+    }
 
-    private int mileage;
-    private int engineSize;
-    private String gearType;
 
     
-   public Car(String make, String model, int year, String color){
-    this.make = make;
-    this.model = model;
-    this.year = year;
-    this.color = color;
-   }
+ 
 
    /*gives out car information */
    public void getCarInfo(){
     System.out.println("The information of this car is as follow" + "\n" +
-    make +   "\n" + model  + "\n" + + year + "\n" + color
+    Vehicle.make +   "\n" + model  + "\n" + + year + "\n" + color
     );
    }
 /*this method sets a new car values */
---
+++ src/main/java/com/example/Vehicle.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Vehicle {
+    private String make;
+    private String model;
+    private int year;
+    
+    public Vehicle(String make, String model, int year){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    
+}
