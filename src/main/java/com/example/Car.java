44cc000c50d39d301015bf45f38a1a5893203653
waqar khan <waqarkhan9@yahoo.com>
2025-02-24 18:19:24 -0800

    car class added

---
+++ src/main/java/com/example/Car.java
@@ -0,0 +1,19 @@
+package com.example;
+
+public class Car {
+    private String make;
+    private String model;
+    private int year;
+    private String color;
+    
+   public Car(String make, String model, int year, String color){
+
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.color = color;
+   }
+
+   
+    
+}
